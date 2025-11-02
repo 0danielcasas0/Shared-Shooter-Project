43ee162b0e8fa1771b2c5e094c71d150e419419a
Voltrix77 <jorger.pacheco@outlook.com>
2025-11-02 08:00:10 -0500

    Multiple Enemy Types
    Added enums to determine enemy type in the enemy script. The second enemy moves in a sine wave pattern. GameManager spawns the second enemy type at a different rate.

--- Enemy.cs
+++ Enemy.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //Establish all enemy types
+    public enum EnemyType
+    {
+        Chaser,
+        Sneaky
+    }
+
+    //Get enemy type
+    public EnemyType theEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
-        if (transform.position.y < -6.5f)
+        //Assign movement based on enemy type
+        switch (theEnemy)
         {
-            Destroy(this.gameObject);
+            case EnemyType.Chaser:
+                transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+                if (transform.position.y < -6.5f)
+                {
+                    Destroy(this.gameObject);
+                }
+                break;
+            case EnemyType.Sneaky:
+                transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 4f);
+                //Sine wave movement
+                transform.Translate(new Vector3(1, 0, 0) * Mathf.Sin(Time.time * 5f + 0f) * 0.025f);
+                if (transform.position.y < -6.5f)
+                {
+                    Destroy(this.gameObject);
+                }
+                break;
         }
     }
 }
